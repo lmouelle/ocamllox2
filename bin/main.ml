9e94b4908fd03fdc5ed1196f17343d9a9a688f95
fake@email.com <fake@email.com>
2023-02-22 22:37:50 -0800

    Impl repl

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+open Ocamllox2.Eval
+
+let rec main env = 
+  try
+    print_string "> ";
+    let ln = read_line () in
+    let value = eval ln |> value_to_string in
+    print_string value;
+    print_newline ();
+    main env
+  with
+  | End_of_file -> exit(0)
+  | InvalidExpression msg -> print_string msg; main env
+
+let () = print_newline (); main []

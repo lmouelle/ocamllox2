42c37652dcc3852a42172cba400edf25d737151b
fake@email.com <fake@email.com>
2023-02-24 13:42:19 -0800

    Adding basic exprs

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,37 +1,18 @@
-type _  gadt_value =
-| Number : float -> float  gadt_value
-| Nil : _ -> _  gadt_value
-| Boolean : bool -> bool  gadt_value
-| String : string -> string  gadt_value
-| Variable : string -> string  gadt_value
-
-type _  gadt_expr =
-| Value : 'a  gadt_value -> 'a  gadt_expr
-| If : bool  gadt_expr * 'a  gadt_expr * 'a  gadt_expr -> 'a  gadt_expr
-| Plus : float  gadt_expr * float  gadt_expr -> float  gadt_expr 
-| Subtract : float  gadt_expr * float  gadt_expr -> float  gadt_expr 
-| Divide : float  gadt_expr * float  gadt_expr -> float  gadt_expr 
-| Multiply : float  gadt_expr * float  gadt_expr -> float  gadt_expr 
-| Or : bool  gadt_expr * bool  gadt_expr -> bool  gadt_expr
-| And : bool  gadt_expr * bool  gadt_expr -> bool  gadt_expr
-(* Instead of seperate rules for equals, lesser, greater, just one to cover them all*)
-| Comparison : 'a  gadt_expr * 'a  gadt_expr * ('a -> 'a -> bool) -> bool  gadt_expr
-
-type untyped_value = 
+type value = 
 | Number of float
 | Nil 
 | Boolean of bool
 | String of string
 | Variable of string
 
-type untyped_expr = 
-| Value of untyped_value
-| If of untyped_expr * untyped_value * untyped_expr
-| Plus of untyped_expr * untyped_expr * untyped_expr 
-| Subtract of untyped_expr * untyped_expr * untyped_expr 
-| Divide of untyped_expr * untyped_expr * untyped_expr 
-| Multiply of untyped_expr * untyped_expr * untyped_expr 
-| Or of untyped_expr * untyped_expr * untyped_expr
-| And  of untyped_expr * untyped_expr * untyped_expr
+type expr = 
+| Value of value
+| If of expr * expr * expr
+| Plus of expr * expr 
+| Subtract of expr * expr 
+| Divide of expr * expr 
+| Multiply of expr * expr 
+| Or of expr * expr
+| And  of expr * expr
 (* Instead of seperate rules for equals, lesser, greater, just one to cover them all*)
-| Comparison : untyped_expr * untyped_expr * ('a -> 'a -> bool) -> untyped_expr
+| Comparison : expr * expr * ('a -> 'a -> bool) -> expr
--- lib/parser.mly
+++ lib/parser.mly
@@ -5,14 +5,18 @@
 %token <float> NUMBER 
 %token <string> IDENTIFIER STRING 
 %token TRUE FALSE
-%token RIGHT_PAREN LEFT_BRACE RIGHT_BRACE COMMA DOT MINUS PLUS SEMICOLON LEFT_PAREN
-%token SLASH STAR BANG BANG_EQUAL EQUAL EQUAL_EQUAL GREATER GREATER_EQUAL 
+%token STAR DIVIDE
+%token PLUS MINUS
+
+%token RIGHT_PAREN LEFT_BRACE RIGHT_BRACE COMMA DOT SEMICOLON LEFT_PAREN
+%token SLASH BANG BANG_EQUAL EQUAL EQUAL_EQUAL GREATER GREATER_EQUAL 
 %token LESS LESS_EQUAL AND CLASS ELSE BOOL FUN FOR 
 %token IF NIL OR PRINT RETURN SUPER THIS VAR WHILE EOF
 %token COMMENT_START COMMENT_END
-%start value
+%start expr
 
-%type <Ast.untyped_value> value
+%type <Ast.value> value
+%type <Ast.expr> expr
 
 %%
 
@@ -24,3 +28,13 @@ value:
 | s = STRING { String s }
 | i = IDENTIFIER { Variable i }
 
+expr:
+| v = value { Value v }
+| IF LEFT_PAREN expr RIGHT_PAREN LEFT_BRACE expr RIGHT_BRACE LEFT_BRACE expr RIGHT_BRACE
+  { If ($3,$6,$9) }
+| expr PLUS expr { Plus ($1,$3) }
+| expr MINUS expr { Subtract ($1,$3) }
+| expr DIVIDE expr { Divide ($1,$3) }
+| expr STAR expr { Multiply ($1,$3) }
+| expr OR expr { Or ($1,$3) }
+| expr AND expr { And ($1,$3) }

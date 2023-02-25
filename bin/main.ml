a4bd6a99abd5d7ef53ad5d5aa12203239ddb9b5f
fake@email.com <fake@email.com>
2023-02-25 13:09:20 -0800

    Add file run functionality

--- bin/main.ml
+++ bin/main.ml
@@ -2,29 +2,32 @@ open Ocamllox2.Parse_utils
 open Ocamllox2.Eval
 open Ocamllox2.Ast
 
-let rec eval_program env exprs = 
-  match exprs with
-  | [] -> {res = Nil; new_env = []}
-  | single :: [] -> eval env single
-  | first :: rest ->
-    let {res = _; new_env} = eval env first in
-    eval_program new_env rest
-
-let rec main env =
+let rec repl env =
   try
     Printf.printf "> ";
     let ln = read_line () in
     let prog = parse_string ln in
-    let {res; new_env} = eval_program env prog in
-    value_to_string res |> print_string; print_newline ();
-    main new_env
-
+    let { res; new_env } = eval_program env prog in
+    value_to_string res |> print_string;
+    print_newline ();
+    repl new_env
   with
   | End_of_file -> exit 0
   | EvalError msg ->
       Printf.printf "%s" msg;
-      main env
+      repl env
 
 let () =
-  print_newline ();
-  main []
+  if
+    Array.length Sys.argv == 1
+    (* No args passed but program name, so repl mode *)
+  then (
+    print_newline ();
+    repl [])
+  else
+    (* Single arg is script name *)
+    let filename = Sys.argv.(1) in
+    let prog = parse_file filename in
+    let { res; new_env = _ } = eval_program [] prog in
+    value_to_string res |> print_string;
+    exit 0

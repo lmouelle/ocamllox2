71e21a1e996b77921a5ae499f50676879780e298
fake@email.com <fake@email.com>
2023-02-25 12:43:44 -0800

    pass environment between repl calls

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,24 @@
 open Ocamllox2.Parse_utils
 open Ocamllox2.Eval
+open Ocamllox2.Ast
+
+let rec eval_program env exprs = 
+  match exprs with
+  | [] -> {res = Nil; new_env = []}
+  | single :: [] -> eval env single
+  | first :: rest ->
+    let {res = _; new_env} = eval env first in
+    eval_program new_env rest
 
 let rec main env =
   try
     Printf.printf "> ";
     let ln = read_line () in
     let prog = parse_string ln in
-    let results = List.rev_map (eval env) prog in
-    match results with
-    | [] -> main env
-    | result :: _ ->
-        value_to_string result.res |> print_string;
-        main result.new_env
+    let {res; new_env} = eval_program env prog in
+    value_to_string res |> print_string; print_newline ();
+    main new_env
+
   with
   | End_of_file -> exit 0
   | EvalError msg ->

2e45886b497b36ca1bf5293f556f1fea370c284a
fake@email.com <fake@email.com>
2023-02-25 19:12:15 -0800

    Fix value_to_string tests for string literals

--- test/eval_test.ml
+++ test/eval_test.ml
@@ -12,8 +12,8 @@ let test_value_to_string _ =
   assert_equal "1." (value_to_string (Number 1.));
   assert_equal "1000.1" (value_to_string (Number 1000.1));
   assert_equal "-0.25" (value_to_string (Number (-0.25)));
-  assert_equal "foo" (value_to_string (String "foo"));
-  assert_equal "" (value_to_string (String ""));
+  assert_equal "\"foo\"" (value_to_string (String "foo"));
+  assert_equal "\"\"" (value_to_string (String ""));
   assert_equal "true" (value_to_string (Boolean true));
   assert_equal "false" (value_to_string (Boolean false));
   assert_equal "nil" (value_to_string Nil)
